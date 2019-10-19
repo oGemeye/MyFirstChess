ef235aec7d37363a2365478a6ed4a4b77ea8d8a2
Daniel <dpsmerdely@gmail.com>
2019-10-19 19:54:59 +1100

    refactoring pieceController and boradDisplay 1/2

--- chess.py
+++ chess.py
@@ -1,11 +1,11 @@
-from pieces import printBoard
+from boardDisplay import printBoard
 
 print("This is my game board :)")
 
 printBoard()
 
-print("and now the other way:")
+# print("and now the other way:")
 
-from pieceController import board
+# from pieceController import board
 
-print("done!")
+# print("done!")
--- pieceController.py
+++ pieceController.py
@@ -1,3 +1,19 @@
+
+class Board:
+    redPieces_Back = ["R1", "Kn1", "B1", "Q", "K", "B2", "Kn2", "R2"]
+    redPieces_Pawn = ["P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8"]
+    bluePieces_Back = ["R1", "Kn1", "B1", "Q", "K", "B2", "Kn2", "R2"]
+    bluePieces_Pawn = ["P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8"]
+    vert = 8
+    hori = 8
+    
+    __init__(self):
+        redPos = [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], 
+                [1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7]]
+        bluePos = [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], 
+                [1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7]]
+
+
 blank = ["" for x in range(8)]
 horizontal = blank
 vertical = [blank for x in range(8)]
